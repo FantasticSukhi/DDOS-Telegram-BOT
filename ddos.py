b390048d88be908ff31d60630979677381698d8d
Raghav124012 <onlyraghavpassi@gmail.com>
2024-11-19 05:09:56 +0530

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -4,11 +4,11 @@ import socket
 import sys
 
 # Telegram API token
-TOKEN = "YOUR TOKEN HERE"
+TOKEN = "8037808972:AAG0kwQnP0g0TQHLpX4WhKCWHwegJ6LI_8s"
 
 
 # Admin chat ID
-ADMIN_CHAT_ID = YOUR_ID_HERE
+ADMIN_CHAT_ID = 7452239135
 
 # Initialize bot
 bot = telebot.TeleBot(TOKEN)
